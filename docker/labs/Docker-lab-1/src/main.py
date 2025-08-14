c59096f49eedcf0af3e4f9fb0645636a0dc8378d
Carlos Argel Arias Cerda <argel.arias90@gmail.com>
2025-08-14 18:52:07 +0000

    add docker-compose to the lab, making /src/ dynamic thanks to that, add more instructions now for compose

--- docker/labs/Docker-lab-1/src/main.py
+++ docker/labs/Docker-lab-1/src/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+debug_mode = os.getenv("DEBUG", False)
 
 @app.route('/')
 def hello():
@@ -8,7 +11,7 @@ def hello():
 
 @app.route('/healthcheck')
 def healthcheck():
-    return "OK"
+    return "<h1>Todo OK</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=debug_mode)
