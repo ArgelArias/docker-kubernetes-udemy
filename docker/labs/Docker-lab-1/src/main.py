a446341e4245ed81f53c3120f6613b544134ce71
Carlos Argel Arias Cerda <argel.arias90@gmail.com>
2025-08-19 02:53:22 +0000

    update compose, and python for inclue a MySQL DB

--- docker/labs/Docker-lab-1/src/main.py
+++ docker/labs/Docker-lab-1/src/main.py
@@ -1,3 +1,4 @@
+import mysql.connector
 from flask import Flask
 import os
 
@@ -12,6 +13,20 @@ def hello():
 @app.route('/healthcheck')
 def healthcheck():
     return "<h1>Todo OK</h1>"
+
+@app.route('/users')
+def get_users():
+    mydb = mysql.connector.connect(
+            host="db",
+            user="user",
+            password="password",
+            database="db"
+            )
+    mycursor = mydb.cursor()
+
+    mycursor.execute("select * from users")
+    result = mycursor.fetchall()
+    return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=debug_mode)
